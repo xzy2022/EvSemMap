0b4a6ca971fecfab4a23fb708036ff584f8eaa6c
xzy2022 <2889354227@qq.com>
2025-12-13 16:44:38 +0800

    [16]使用00001序列进行训练

--- EvSemSeg/datasets/loader_rellis.py
+++ EvSemSeg/datasets/loader_rellis.py
@@ -21,7 +21,8 @@ rellis_split_dict = {
 	"-1": ['00000', '00002', '00003', '00004'],
 	"-2": ['00000', '00001', '00003', '00004'],
 	"-3": ['00000', '00001', '00002', '00004'],
-	"-4": ['00000', '00001', '00002', '00003'],
+	# "-4": ['00000', '00001', '00002', '00003'],
+	"-4": ['00001'],
 	"0": ['00000'],
 	"1": ['00001'],
 	"2": ['00002'],
@@ -32,7 +33,7 @@ rellis_split_dict = {
 LOADER_PHASE = ['train', 'val', 'test']
 # RELLIS_ROOT = '/data/Rellis-3D'
 # RELLIS_ROOT = '/home/xzy/datasets/Rellis-3D'
-RELLIS_ROOT = '~/datasets/Rellis-3D'
+RELLIS_ROOT = '/root/datasets/Rellis-3D'
 RGB_POSTFIX = 'pylon_camera_node'
 LBL_POSTFIX = 'pylon_camera_node_label_color'
 
